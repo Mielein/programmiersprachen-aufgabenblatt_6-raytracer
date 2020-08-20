28b0fbffbf9a71b328b52f48543f8eb81fe0b62e
Mielein <marie.trojan@uni-weimar.de>
2020-08-20 21:53:16 +0200

    Revert "konstruktoren für die kamera"
    This reverts commit 1d9dfdefb3b6c99e16c1a890d3278a0d2d27542a.

--- framework/camera.cpp
+++
@@ -1,8 +0,0 @@
-#include "camera.hpp"
-
-Camera::Camera():
-Camera::Camera(glm::vec3 pos, glm::vec3 direction):
-    pos_{pos},
-    direction_{direction}{}
-Camera::Camera(std::string, glm::vec3 pos, glm::vec3 direction):
-    {}
--- framework/camera.hpp
+++ framework/camera.hpp
@@ -6,8 +6,7 @@
 
 class Camera{
 public:
-    Camera(glm::vec3 pos, glm::vec3 direction);
-    Camera(std::string name,glm::vec3 pos, glm::vec3 direction);
+    Camera();
 private:
     std::string name_{" "};
     glm::vec3 pos_{0.0f,0.0f,0.0f};
