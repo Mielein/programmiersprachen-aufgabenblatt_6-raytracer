2d545bb820cd9731fe2beb9d8021f66dafc68db5
Mielein <marie.trojan@uni-weimar.de>
2020-08-20 21:54:49 +0200

    Revert "edit camera"
    This reverts commit a385a02c3a2ddcc1db8b074b9828951e02183eba.

--- framework/camera.hpp
+++ framework/camera.hpp
@@ -4,13 +4,11 @@
 #include <glm/vec3.hpp>
 #include <string>
 
-class Camera{
-public:
-    Camera();
-private:
+struct Camera{
     std::string name_{" "};
     glm::vec3 pos_{0.0f,0.0f,0.0f};
     glm::vec3 direction_{0.0f,0.0f,0.0f};
+    
 };
 
 
