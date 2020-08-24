ed886e609dd277b4f1467ceea06d04e16b5fc3ae
Mielein <marie.trojan@uni-weimar.de>
2020-08-24 12:00:03 +0200

    added light

--- framework/sdfParser.cpp
+++ framework/sdfParser.cpp
@@ -2,6 +2,7 @@
 #include <fstream> //open / read / write / close files
 #include <sstream> //stringstram -> easy parsing mechanics
 #include <string> //buffer lines read from file
+#include <glm/vec3.hpp>
 
 int main(int argc, char** argv){
 
@@ -23,12 +24,9 @@ int main(int argc, char** argv){
 
     while(std::getline(in_file, line_buffer)){
         //std::cout<<++line_count<<": "<<line_buffer<<std::endl;
-
         //construct stringstream using line_buffer string
         std::istringstream in_sstream(line_buffer);
-
         in_sstream>>identifier;
-
         std::cout<<"Identifier content: "<<identifier<<std::endl;
 
         //check for shapes / material / lights
@@ -39,6 +37,9 @@ int main(int argc, char** argv){
         else if("material" == class_name){
             //parse material attributes
             std::string material_name;
+            //ka -> ambiente
+            //kd -> spiegeld
+            //ks -> reflexion
             float ka_red, ka_green, ka_blue;
             float kd_red, kd_green, kd_blue;
             float ks_red, ks_green, ks_blue;
@@ -51,6 +52,22 @@ int main(int argc, char** argv){
         }
         else if("light" == class_name){
             //TO-DO
+            std::string light_name;
+            in_sstream >> light_name;
+
+            float clr_r, clr_g, clr_b;
+            glm::vec3 pos;
+            float brightness;
+
+            in_sstream >> clr_r;
+            in_sstream >> clr_g;
+            in_sstream >> clr_b;
+            in_sstream >> pos.x;
+            in_sstream >> pos.y;
+            in_sstream >> pos.z;
+            in_sstream >> brightness;
+
+
         }
         else {
             std::cout<<"Line was not valid!"<<std::endl;
