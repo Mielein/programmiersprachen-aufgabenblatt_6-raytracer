daf4dd68198170439cccf1dc9bc722f20d11afd3
Mielein <marie.trojan@uni-weimar.de>
2020-08-25 19:47:12 +0200

    vitual intersect method implementes

--- framework/shape.hpp
+++ framework/shape.hpp
@@ -4,6 +4,8 @@
 #include <string>
 #include "color.hpp"
 #include "material.hpp"
+#include "hitpoint.hpp"
+#include "ray.hpp"
 #include <iostream>
 #include <memory>
 
@@ -15,6 +17,7 @@ class Shape{
     virtual ~Shape();
     virtual float area() const = 0;
     virtual float volume() const = 0;
+    virtual HitPoint intersect(Ray const& ray) const = 0;
     virtual std::ostream& print( std::ostream& os) const;
 
     protected:
