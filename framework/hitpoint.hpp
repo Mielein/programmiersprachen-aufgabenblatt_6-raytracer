1d60ce2c1b69cedd0f9fab5412dbe56bfbb42f09
Mielein <marie.trojan@uni-weimar.de>
2020-08-26 16:17:00 +0200

    added some

--- framework/hitpoint.hpp
+++ framework/hitpoint.hpp
@@ -2,6 +2,7 @@
 #define HITPOINT_HPP
 
 #include <glm/vec3.hpp>
+#include <glm/glm.hpp>
 #include <string>
 #include "color.hpp"
 
@@ -16,10 +17,10 @@ struct HitPoint{
     glm::vec3 normal_;
 
     //intersect_pt and normal to world space
-    void world_transform(glm::mat4 const& mat, glm::mat4 const& inv_mat){
+/*     void world_transform(glm::mat4 const& mat, glm::mat4 const& inv_mat){
         intersect_pt_ = glm::vec3(mat*glm::vec4(intersect_pt_, 1.0f));
         normal_= glm::normalize(glm::vec3(inv_mat*glm::vec4(normal_, 0.0f)));
-    }
+    } */
 };
 
 
