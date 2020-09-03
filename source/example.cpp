2fd27579826495f7cc3a3d55e1efc58e06af88c0
YoFionn <fionn.edward.erickson@uni-weimar.de>
2020-09-03 22:49:57 +0200

    Dudelidu ein Commit for You. :3

--- source/example.cpp
+++ source/example.cpp
@@ -28,14 +28,14 @@ int main(int argc, char* argv[]){
   mat_map.insert(std::make_pair("Orange", orange_ptr));
   mat_map.insert(std::make_pair("Random", random_ptr));
 
-  Sphere sphere1{"pussy",{0.0f,0.0f,1.0f}, green_ptr,{0.0f,0.0f,-100.0f}, 5.0f};
-  Sphere sphere2{"pimpim",{1.0f,0.0f,0.0f}, orange_ptr,{0.0f,7.5f,-100.0f}, 5.0f};
+  //Sphere sphere1{"pussy",{0.0f,0.0f,1.0f}, green_ptr,{0.0f,0.0f,-100.0f}, 5.0f};
+  //Sphere sphere2{"pimpim",{1.0f,0.0f,0.0f}, orange_ptr,{0.0f,7.5f,-100.0f}, 5.0f};
   Box box1{{-10.0f,-10.0f,-150.0f},{5.0f,5.0f,-96.0f},"jucyButt",{0.0f,0.0f,0.0f},random_ptr};
-  std::shared_ptr<Shape>spheres1 = std::make_shared<Sphere>(sphere1);
-  std::shared_ptr<Shape>spheres2 = std::make_shared<Sphere>(sphere2);
+  //std::shared_ptr<Shape>spheres1 = std::make_shared<Sphere>(sphere1);
+  //std::shared_ptr<Shape>spheres2 = std::make_shared<Sphere>(sphere2);
   std::shared_ptr<Shape>boxes1 = std::make_shared<Box>(box1);
-  shape_vec.push_back(spheres1);
-  shape_vec.push_back(spheres2);
+  //shape_vec.push_back(spheres1);
+  //shape_vec.push_back(spheres2);
   shape_vec.push_back(boxes1);
 
   for(auto i : shape_vec){
