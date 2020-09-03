2c7f9ee0e70ef258ab74b03644dc2e0679f0595f
Mielein <marie.trojan@uni-weimar.de>
2020-09-03 12:48:51 +0200

    WARUM GEHT DIESE RECHNUNG NICHT???git add framework source!

--- source/example.cpp
+++ source/example.cpp
@@ -18,7 +18,7 @@ int main(int argc, char* argv[]){
   std::vector<std::shared_ptr<Shape>> shape_vec; 
   std::vector<std::shared_ptr<Light>> light_vec;
   std::map<std::string,std::shared_ptr<Material>> mat_map;
-  Sphere sphere{{0.0,0.0,-1000.0}, 5.0f ,"pussy",{0.0,0.0,1.0}};
+  Sphere sphere{{500.0,500.0,-1000.0}, 50.0f ,"pussy",{0.0,0.0,1.0}};
   std::shared_ptr<Shape>spheres = std::make_shared<Sphere>(sphere);
   shape_vec.push_back(spheres);
   Light light{};
@@ -29,9 +29,9 @@ int main(int argc, char* argv[]){
   Scene s{shape_vec,light_vec,mat_map,cam,amby};
   /* sdfParser("/home/marie/programmiersprachen-aufgabenblatt_6-raytracer/source/example.sdf"); */
 
-  for (auto a : s.shape_vec){
+/*   for (auto a : s.shape_vec){
     std::cout<< *a << std::endl;
-  }
+  } */
   
   std::cout<<"sdf read"<<std::endl;
   renderer.render(s);
